4c90004fc8f39b2dc6ba52842f645f607c19f2c1
sasalilime <salimtison@gmail.com>
2021-09-14 17:53:00 +0200

    Architecture

--- includes/header.php
+++ includes/header.php
@@ -1,1 +1,9 @@
-<header class="logo">Suits Todo</header>
+<header>
+
+    <a href="/" class="logo">Suits Blog</a>
+    <ul class="header-menu">
+        <li>
+            <a href="/add-article.php">Ecrire un article</a>
+        </li>
+    </ul>
+</header>
