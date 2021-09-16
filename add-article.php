3e1a2da40d2d7a280e8a75287ae48073dae95cd6
sasalilime <salimtison@gmail.com>
2021-09-16 10:19:38 +0200

    Error sanitize

--- add-article.php
+++ add-article.php
@@ -1,3 +1,41 @@
+<?php
+
+const ERROR_REQUIRED = 'Veuillez renseigner ce champ.';
+const ERROR_TITLE_TOO_SHORT = 'Le titre est trop court';
+const ERROR_CONTENT_TOO_SHORT = "L'article est trop court";
+const ERROR_IMAGE_URL = "L'image doit être une url valide";
+
+$errors = [
+    'title' => '',
+    'image' => '',
+    'category' => '',
+    'content' => ''
+];
+
+if ($_SERVER('REQUEST_METHOD') == 'POST') {
+    $_POST = filter_input_array(INPUT_POST, [
+        'title' => FILTER_SANITIZE_STRING,
+        'image' => FILTER_SANITIZE_URL,
+        'category' => FILTER_SANITIZE_STRING,
+        'content' => [
+            'filter' => FILTER_SANITIZE_STRING,
+            'flag' => FILTER_FLAG_NO_ENCODE_QUOTES
+        ]
+    ]);
+
+    $title = $_POST['title'] ?? '';
+    $image = $_POST['image'] ?? '';
+    $category = $_POST['category'] ?? '';
+    $content = $_POST['content'] ?? '';
+}
+
+
+
+
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -12,7 +50,7 @@
     <div class="container">
         <?php require_once 'includes/header.php' ?>
         <div class="content">
-            <div class="block p-20">
+            <div class="block p-20 form-container">
                 <h1>Ecrire un article </h1>
                 <form action="/add-article.php" method="POST">
                     <div class="form-control">
@@ -40,9 +78,9 @@
                         <textarea type="text" name="content" id="content"></textarea>
                         <p class="text-error"></p>
                     </div>
-                    <div class="form-action">
-                        <button class="btn btn-secondary">Annuler</button>
-                        <button class="btn btn-primary">Sauvergarder</button>
+                    <div class="form-actions">
+                        <a href="/" class="btn btn-secondary">Annuler</a>
+                        <button class="btn btn-primary">Sauvegarder</button>
                     </div>
                 </form>
 
