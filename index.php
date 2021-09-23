bed3f0367f5a77f50069aa005d4da02574daa792
sasalilime <salimtison@gmail.com>
2021-09-23 15:31:40 +0200

    Display article by id

--- index.php
+++ index.php
@@ -59,12 +59,12 @@ if (file_exists($filename)) {
                             <h2> <?= $cat ?> </h2>
                             <div class="articles-container">
                                 <?php foreach ($articlePerCategories[$cat] as $article) : ?>
-                                    <div class="article block">
+                                    <a href="/show-article.php?id=<?= $article['id'] ?>" class="article block">
                                         <div class="overflow">
                                             <div class="img-container" style="background-image:url(<?= $article['image']  ?>)"></div>
                                         </div>
                                         <h3><?= $article['title'] ?></h3>
-                                    </div>
+                                    </a>
                                 <?php endforeach; ?>
                             </div>
                         <?php endforeach ?>
@@ -72,12 +72,12 @@ if (file_exists($filename)) {
                         <h2><?= $selectedCat  ?></h2>
                         <div class="articles-container">
                             <?php foreach ($articlePerCategories[$selectedCat] as $article) : ?>
-                                <div class="article block">
+                                <a href="/show-article.php?id=<?= $article['id'] ?>" class=" article block">
                                     <div class="overflow">
                                         <div class="img-container" style="background-image:url(<?= $article['image']  ?>)"></div>
                                     </div>
                                     <h3><?= $article['title'] ?></h3>
-                                </div>
+                                </a>
                             <?php endforeach; ?>
                         </div>
                     <?php endif; ?>
