0fb69bd406d61b5892a3485dbcf3ee002d2fd0c1
salim <salimtison@gmail.com>
2021-09-27 16:09:14 +0200

    connexion mysql

---
+++ data/seed-articles.php
@@ -0,0 +1,35 @@
+<?php
+
+
+$articles = json_decode(file_get_contents('./articles.json'), true);
+
+
+$dns = 'mysql:host=localhost;dbname=blog';
+$user = 'salim';
+$pwd = 'Suits123!';
+
+
+$pdo = new Pdo($dns, $user, $pwd);
+
+
+$statement = $pdo->prepare('INSERT INTO 
+article (
+      title,
+      category,
+      content,
+      image
+) VALUES (
+ :title,
+ :category, 
+ :content, 
+ :image
+ ) ');
+
+
+foreach ($articles as $article) {
+      $statement->bindValue(':title', $article['title']);
+      $statement->bindValue(':category', $article['category']);
+      $statement->bindValue(':content', $article['content']);
+      $statement->bindValue(':image', $article['image']);
+      $statement->execute();
+}
