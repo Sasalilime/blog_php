e2a3671c7bbe7367859f368f140f7bcc492ba9cf
salim <salimtison@gmail.com>
2021-10-10 01:51:35 +0200

    current user

--- includes/header.php
+++ includes/header.php
@@ -1,21 +1,30 @@
+<?php
+$currentUser = $currentUser ?? false;
+?>
+
+
 <header>
     <a href="/" class="logo">Suits Blog</a>
     <ul class="header-menu">
-        <li class=<?= $_SERVER['REQUEST_URI'] == '/form-article.php' ? 'active' : '' ?>>
-            <a href="/form-article.php">Ecrire un article</a>
-        </li>
-        <li class=<?= $_SERVER['REQUEST_URI'] == '/auth-register.php' ? 'active' : '' ?>>
-            <a href="/auth-register.php">Inscription</a>
-        </li>
-        <li class=<?= $_SERVER['REQUEST_URI'] == '/auth-login.php' ? 'active' : '' ?>>
-            <a href="/auth-login.php">Connexion</a>
-        </li>
-        <li>
-            <a href="/auth-logout.php">Deconnexion</a>
-        </li>
-        <li class=<?= $_SERVER['REQUEST_URI'] == '/profile.php' ? 'active' : '' ?>>
-            <a href="/profile.php">Mon profil</a>
-        </li>
+        <?php if ($currentUser) : ?>
+            <li class=<?= $_SERVER['REQUEST_URI'] == '/form-article.php' ? 'active' : '' ?>>
+                <a href="/form-article.php">Ecrire un article</a>
+            </li>
+            <li>
+                <a href="/auth-logout.php">Deconnexion</a>
+            </li>
+            <li class=<?= $_SERVER['REQUEST_URI'] == '/profile.php' ? 'active' : '' ?>>
+                <a href="/profile.php">Mon profil</a>
+            </li>
+        <?php else : ?>
 
+            <li class=<?= $_SERVER['REQUEST_URI'] == '/auth-register.php' ? 'active' : '' ?>>
+                <a href="/auth-register.php">Inscription</a>
+            </li>
+            <li class=<?= $_SERVER['REQUEST_URI'] == '/auth-login.php' ? 'active' : '' ?>>
+                <a href="/auth-login.php">Connexion</a>
+            </li>
+
+        <?php endif; ?>
     </ul>
 </header>
